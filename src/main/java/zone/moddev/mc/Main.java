0514e7648f44fd8aa6053cda5727f55b1286026c
KiriCattus <19393068+KiriCattus@users.noreply.github.com>
2026-08-15 01:55:59 +0100

    Update to 1.11
    Signed-off-by: KiriCattus <19393068+KiriCattus@users.noreply.github.com>

--- src/main/java/zone/moddev/mc/Main.java
+++
@@ -1,6 +0,0 @@
-package zone.moddev.mc;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
---
+++ src/main/java/zone/moddev/mc/mmdmodtemplate/MMDModTemplate.java
@@ -0,0 +1,13 @@
+package zone.moddev.mc.mmdmodtemplate;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = MMDModTemplate.MOD_ID, name = MMDModTemplate.MOD_NAME)
+public class MMDModTemplate {
+
+    public static final String MOD_ID = "mmdmodtemplate";
+    public static final String MOD_NAME = "MMD Mod Template";
+
+    public static void main(String[] args) {
+    }
+}
